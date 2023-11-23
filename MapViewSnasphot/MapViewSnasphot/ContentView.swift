02dac8d16f548988cbe7b2962c5fd2f2a78d8a5c
finebel <46824694+finebel@users.noreply.github.com>
2023-11-23 20:27:09 +0100

    Add demo project

---
+++ MapViewSnasphot/MapViewSnasphot/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  MapViewSnasphot
+//
+//  Created by Finn Ebeling on 23.11.23.
+//
+
+import SwiftUI
+import MapKit
+
+struct Item: Identifiable {
+    let id = UUID()
+    let coord: CLLocationCoordinate2D
+}
+
+struct ContentView: View {
+    private let items = createItems()
+    
+    var body: some View {
+        Map(
+            coordinateRegion: .constant(MKCoordinateRegion(MKMapRect.world)),
+            annotationItems: items
+        ) { item in
+            MapMarker(coordinate: item.coord)
+        }
+    }
+    
+    private static func createItems() -> [Item] {
+        var res: [Item] = []
+        stride(from: -50, through: 50, by: 5).forEach { lat in
+            stride(from: -50, through: 100, by: 5).forEach { long in
+                res.append(Item(coord: CLLocationCoordinate2D(latitude: lat, longitude: long)))
+            }
+        }
+        
+        return res
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapViewSnasphot/MapViewSnasphot/MapViewSnasphotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewSnasphotApp.swift
+//  MapViewSnasphot
+//
+//  Created by Finn Ebeling on 23.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MapViewSnasphotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
